12f5eabbe9021e15b41be228782fa67dc6a609bf
liu <602694077@qq.com>
2017-12-01 10:27:23 +0800

    Initial commit

---
+++ app/src/main/java/example/ldgd/com/checkjni/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package example.ldgd.com.checkjni.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import example.ldgd.com.checkjni.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
